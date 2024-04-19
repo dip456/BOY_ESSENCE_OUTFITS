54031053fa593ff29077a3f4f4a2203c19a053cf
dip456 <dipchaudhari808@gmail.com>
2024-04-19 23:39:38 +0530

    login template

--- buyer/urls.py
+++ buyer/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 
 from.views import base_page_view
+from.views import login_page_view
 
 urlpatterns = [
     path('',base_page_view,name='base_page_view'),
+    path('login/',login_page_view,name='login_page_view')
 ]
--- buyer/views.py
+++ buyer/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def base_page_view(request):
     return render(request,'buyer/base.html')
+
+
+def login_page_view(request):
+    return render(request,'buyer/login.html')
